adf3f8965a8ba7b5c71dd30b0dbec1703e517a2e
kbrsrkl1 <kubranur216@gmail.com>
2023-07-16 17:25:50 +0300

    commit

--- dictionaray/src/Result.js
+++ dictionaray/src/Result.js
@@ -3,9 +3,26 @@ import React from 'react'
 const Result = () => {
     return (
         <ul>
-            <li className='world'>
+            <li className='word'>
                 <h2>Hello</h2>
-                <span> </span>
+                <span>həˈlō</span>
+            </li>
+
+            <li className="contain">
+                <h3>noun</h3>
+
+                <div className="details meaning">
+                    <h3>Meaning</h3>
+                    <p>- "Hello!" or an equivalent greeting.</p>
+                </div>
+
+
+                <div className="details synonyms">
+                    <h3>Synonyms</h3>
+                    <span>greeting, greeting, greeting</span>
+                </div>
+            
+            
             </li>
         </ul>
     )
